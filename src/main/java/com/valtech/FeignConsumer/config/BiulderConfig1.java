e14782d18cc7c74fa6ecbdec0f61037388566162
zabikhanjava <ismail.zabiulla@valtech.com>
2022-01-03 11:36:27 +0530

    mycommit

---
+++ src/main/java/com/valtech/FeignConsumer/config/BiulderConfig1.java
@@ -0,0 +1,16 @@
+package com.valtech.FeignConsumer.config;
+
+import com.valtech.FeignConsumer.service.ConsumeProductService;
+import feign.Feign;
+import feign.Logger;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.okhttp.OkHttpClient;
+import feign.slf4j.Slf4jLogger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+public class BiulderConfig1 {
+
+}
